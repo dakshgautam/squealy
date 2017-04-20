a7c2dcb74337e9e7e90991572feff824ff983ae5
dakshgautam <daksh.gautam@hashedin.com>
2017-04-20 18:50:46 +0530

    Check ti

--- squealy/views.py
+++ squealy/views.py
@@ -49,9 +49,10 @@ class DatabaseView(APIView):
             print (database)
             for db in database:
                 # if db != 'default':db
+                print (db, 'There is database')
                 database_response.append({
                   'value': db,
-                  'label': database[db]['OPTIONS']['display_name'] if 'display_name' in database[db]['OPTIONS'] else db
+                  'label': database[db]['OPTIONS']['display_name'] if 'OPTIONS' in database[db] and 'display_name' in database[db]['OPTIONS'] else db
                 })
             if not database_response:
                 raise DatabaseConfigurationException('No databases found. Make sure that you have defined database configuration in django admin')
